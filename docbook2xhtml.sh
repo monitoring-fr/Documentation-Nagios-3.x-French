a6f95aacfd1ec19736bdc98a8483380fab42991b
admin <admin>
2010-03-21 10:14:20 +0000

    Modification de la generation xhtml.

--- docbook2xhtml.sh
+++ docbook2xhtml.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-rm -R xhtml/*
-cp style.css xhtml/
-rsync -avP --exclude=.svn images xhtml/
-xmllint --xinclude --postvalid  fr_FR.nagios.xml --output fr_FR.nagios-big.xml
-xsltproc --output xhtml/ --xinclude xhtml.xsl fr_FR.nagios-big.xml
-rm fr_FR.nagios-big.xml
+OUT_PATH=html
+IN_XML=fr_FR.nagios-big.xml
+
+rm -R $OUT_PATH/*
+mkdir $OUT_PATH/includes
+cp web_common/style.css $OUT_PATH/
+rsync -avP --exclude=.svn images $OUT_PATH/
+rsync -avP web_common/images $OUT_PATH/includes/
+rsync -avP web_common/css $OUT_PATH/includes/
+xmllint --xinclude --postvalid  fr_FR.nagios.xml --output $IN_XML
+xsltproc --output $OUT_PATH/ --xinclude web_common/xhtml.xsl $IN_XML
+rm $IN_XML
