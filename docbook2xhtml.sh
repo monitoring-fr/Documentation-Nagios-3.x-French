cbe7f2fcbf34138be9c0ede66588bda48d99111c
admin <admin>
2010-05-29 12:21:18 +0000

    Generation pdf et html de la doc beta 7. Inclusion du script de pilotage de htmltidy dans docbook2xhtml.sh.

--- docbook2xhtml.sh
+++ docbook2xhtml.sh
@@ -7,8 +7,20 @@ rm -fR $OUT_PATH/*
 mkdir $OUT_PATH/includes
 cp web_common/style.css $OUT_PATH/
 rsync -avP --exclude=.svn images $OUT_PATH/
-rsync -avP web_common/images $OUT_PATH/includes/
-rsync -avP web_common/css $OUT_PATH/includes/
+rsync -avP --exclude=.svn web_common/images $OUT_PATH/includes/
+rsync -avP --exclude=.svn web_common/css $OUT_PATH/includes/
 xmllint --xinclude --postvalid  fr_FR.nagios.xml --output $IN_XML
 xsltproc --output $OUT_PATH/ --xinclude web_common/xhtml.xsl $IN_XML
+
+# html tidy magic
+
+mkdir $OUT_PATH/tidy
+cd $OUT_PATH
+for file in `ls *.html`
+
+do
+/usr/bin/tidy -config ../tidy.cfg $file > tidy/$file
+done
+
+# cleaning
 rm $IN_XML
