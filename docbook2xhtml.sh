550e8792dfee5bfc7a608c7179e5520d910ef817
Olivier JAN <ojan@monitoring-fr.org>
2012-02-08 19:46:44 +0100

    Corrections et améliorations diverses sur sortie HTML

--- docbook2xhtml.sh
+++ docbook2xhtml.sh
@@ -2,6 +2,9 @@
 
 OUT_PATH=html
 IN_XML=fr_FR.nagios-big.xml
+XMLLINT=`which xmllint`
+XSLTPROC=`which xsltproc`
+TIDY=`which tidy`
 
 rm -fR $OUT_PATH/*
 mkdir $OUT_PATH/includes
@@ -9,8 +12,8 @@ cp web_common/style.css $OUT_PATH/
 rsync -avP --exclude=.svn images $OUT_PATH/
 rsync -avP --exclude=.svn web_common/images $OUT_PATH/includes/
 rsync -avP --exclude=.svn web_common/css $OUT_PATH/includes/
-xmllint --xinclude --postvalid  fr_FR.nagios.xml --output $IN_XML
-xsltproc --output $OUT_PATH/ --xinclude web_common/xhtml.xsl $IN_XML
+$XMLLINT --xinclude --postvalid  fr_FR.nagios.xml --output $IN_XML
+$XSLTPROC --output $OUT_PATH/ --xinclude web_common/xhtml.xsl $IN_XML
 
 # html tidy magic
 
@@ -19,7 +22,7 @@ cd $OUT_PATH
 for file in `ls *.html`
 
 do
-/usr/bin/tidy -config ../tidy.cfg $file > tidy/$file
+$TIDY -config ../tidy.cfg $file > tidy/$file
 done
 
 cd ../
--- web_common/xhtml.xsl
+++ web_common/xhtml.xsl
@@ -99,10 +99,6 @@
 
       <xsl:call-template name="user.footer.navigation"/>
     </div>
-	 	<script type="text/javascript">
-			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	 	</script>
 		<!-- Piwik -->
 		<script type="text/javascript">
 		var pkBaseURL = (("https:" == document.location.protocol) ? "https://analytics.monitoring-fr.org/" : "http://analytics.monitoring-fr.org/");
